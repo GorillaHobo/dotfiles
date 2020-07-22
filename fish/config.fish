22a07ec1ad0bea0113448baf3af48ae251db8d5a
gorillahobo <galihwicaksono@gmail.com>
2020-07-22 09:53:42 +0700

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,31 @@
+#alises
+
+# Launch neovim
+alias v="nvim"
+
+# Neovim configuration
+alias vfish="nvim ~/.config/fish/config.fish"
+alias vbsp="nvim ~/.config/bspwm/bspwmrc"
+alias vsx="nvim ~/.config/sxhkd/sxhkdrc"
+alias vala="nvim ~/.config/alacritty/alacritty.yml"
+alias vvim="nvim ~/.config/nvim/init.vim"
+
+# nnn aliases 
+alias n="nnn"
+alias ndown="nnn ~/Storage/Downloads/"
+alias nconf="nnn ~/.config/"
+alias ns="nnn ~/Storage/"
+alias nd="nnn ~/Documents/"
+alias ndocs="nnn ~/Storage/Documents/"
+alias yamaha="nnn ~/Storage/Documents/Yamaha/"
+alias nex="nnn /run/media/tony/"
+alias npoly="nnn ~/.config/polybar/"
+alias nmtp="nnn ~/.mtp/"
+
+# mount usb
+alias usbmount="udisksctl mount -b /dev/sdc1"
+alias usbunmount="udisksctl unmount -b /dev/sdc1"
+
+# mount phone mtp
+alias mtpmount="jmtpfs ~/.mtp"
+alias mtpunmount="fusermount -u ~/.mtp"
---
+++ nvim/init.vim
@@ -0,0 +1,14 @@
+call plug#begin()
+"Plug 'scrooloose/nerdcommenter'
+Plug 'tpope/vim-commentary'
+Plug 'tpope/vim-surround'
+Plug 'itchyny/lightline.vim'
+Plug 'morhetz/gruvbox'
+call plug#end()
+
+
+let mapleader=" "
+set number! relativenumber!
+imap jj <Esc>
+imap <C-f> <Esc>la
+imap <C-b> <Esc>ha
